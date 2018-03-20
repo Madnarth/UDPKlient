a8c253dcee213327e671e9d73fffc2d16175a40c
Madnarth <corwyn.blade@gmail.com>
2018-03-20 21:08:23 +0100

    Initial commit

---
+++ UDPKlient/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace UDPKlient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bSend = new System.Windows.Forms.Button();
+            this.lbLogs = new System.Windows.Forms.ListBox();
+            this.numPort = new System.Windows.Forms.NumericUpDown();
+            this.tbHostAddress = new System.Windows.Forms.TextBox();
+            this.tbText = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Adres:";
+            // 
+            // bSend
+            // 
+            this.bSend.Location = new System.Drawing.Point(155, 176);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(75, 23);
+            this.bSend.TabIndex = 9;
+            this.bSend.Text = "Wyślij";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.bConnect_Click);
+            // 
+            // lbLogs
+            // 
+            this.lbLogs.FormattingEnabled = true;
+            this.lbLogs.Location = new System.Drawing.Point(12, 75);
+            this.lbLogs.Name = "lbLogs";
+            this.lbLogs.Size = new System.Drawing.Size(360, 95);
+            this.lbLogs.TabIndex = 8;
+            // 
+            // numPort
+            // 
+            this.numPort.Location = new System.Drawing.Point(307, 19);
+            this.numPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numPort.Name = "numPort";
+            this.numPort.Size = new System.Drawing.Size(66, 20);
+            this.numPort.TabIndex = 7;
+            // 
+            // tbHostAddress
+            // 
+            this.tbHostAddress.Location = new System.Drawing.Point(55, 19);
+            this.tbHostAddress.Name = "tbHostAddress";
+            this.tbHostAddress.Size = new System.Drawing.Size(211, 20);
+            this.tbHostAddress.TabIndex = 6;
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(12, 49);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(360, 20);
+            this.tbText.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 211);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bSend);
+            this.Controls.Add(this.lbLogs);
+            this.Controls.Add(this.numPort);
+            this.Controls.Add(this.tbHostAddress);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bSend;
+        private System.Windows.Forms.ListBox lbLogs;
+        private System.Windows.Forms.NumericUpDown numPort;
+        private System.Windows.Forms.TextBox tbHostAddress;
+        private System.Windows.Forms.TextBox tbText;
+    }
+}
+
---
+++ UDPKlient/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDPKlient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bConnect_Click(object sender, EventArgs e)
+        {
+            string host = tbHostAddress.Text;
+            int port = (int)numPort.Value;
+            try
+            {
+                UdpClient klient = new UdpClient(host, port);
+                Byte[] dane = Encoding.ASCII.GetBytes(tbText.Text);
+                klient.Send(dane, dane.Length);
+                lbLogs.Items.Add("Wysłanie wiadomości do hosta " + host +":" + port);
+                klient.Close();
+            }
+            catch (Exception ex)
+            {
+                lbLogs.Items.Add("Błąd: Nie udało się wysłać wiadomości!");
+                MessageBox.Show(ex.ToString(), "Błąd");
+            }
+        }
+    }
+}
